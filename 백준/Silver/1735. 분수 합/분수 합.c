c926fcab4152bb40382080386a87a12f5ddb282d
jin616 <107658586+Jin616@users.noreply.github.com>
2023-03-27 13:06:21 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.c
+++ 백준/Silver/1735. 분수 합/분수 합.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 
-int main() {
-    int num[2][2], tem, i = 2;
-    scanf("%d %d\n%d %d", &num[0][0], &num[0][1], &num[1][0], &num[1][1]);
-    tem = num[0][1];
-    num[0][0] *= num[1][1];
-    num[0][1] *= num[1][1];
-    num[0][0] += num[1][0] * tem;
-    while (i <= num[0][0] && i <= num[0][1]) {
-        if (num[0][0] % i == 0 && num[0][1] % i == 0) {
-            num[0][0] /= i;
-            num[0][1] /= i;
-        } else {
-            i++;
-        }
+void swap(int *a, int *b) {
+    int tem;
+    tem = *a;
+    *a = *b;
+    *b = tem;
+}
+
+int func(int a, int b) {
+    while (1) {
+        if (a < b)
+            swap(&a, &b);
+        if (a % b == 0)
+            break;
+        else
+            a %= b;
     }
-    printf("%d %d", num[0][0], num[0][1]);
+    return (b);
+}
+
+int main() {
+    int a[2], b[2], tem;
+    scanf("%d %d\n%d %d", &a[0], &a[1], &b[0], &b[1]);
+    tem = func(a[1], b[1]);
+    a[0] = (a[0] * b[1] + b[0] * a[1]) / tem;
+    a[1] *= b[1] / tem;
+    tem = func(a[0], a[1]);
+    printf("%d %d", a[0] / tem, a[1] / tem);
     return 0;
 }
