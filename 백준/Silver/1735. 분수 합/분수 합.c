6efcf58b869d98a82ae7eb081eb12ecfe5dcbaf1
jin616 <107658586+Jin616@users.noreply.github.com>
2023-03-27 13:17:30 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.c
+++ 백준/Silver/1735. 분수 합/분수 합.c
@@ -1,31 +1,15 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int tem;
-    tem = *a;
-    *a = *b;
-    *b = tem;
-}
-
-int func(int a, int b) {
-    while (1) {
-        if (a < b)
-            swap(&a, &b);
-        if (a % b == 0)
-            break;
-        else
-            a %= b;
-    }
-    return (b);
-}
-
 int main() {
-    int a[2], b[2], tem;
-    scanf("%d %d\n%d %d", &a[0], &a[1], &b[0], &b[1]);
-    tem = func(a[1], b[1]);
-    a[0] = (a[0] * b[1] + b[0] * a[1]) / tem;
-    a[1] *= b[1] / tem;
-    tem = func(a[0], a[1]);
-    printf("%d %d", a[0] / tem, a[1] / tem);
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    a = c = a * d + c * b;
+    b = d = b * d;
+    while (c * d)
+        if (c > d)
+            c %= d;
+        else
+            d %= c;
+    printf("%d %d", a / (c + d), b / (c + d));
     return 0;
 }
