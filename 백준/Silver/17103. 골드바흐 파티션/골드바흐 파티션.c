24dd58910ae2fe261e94b2f5d2c63e43b0f30dd7
jin616 <107658586+Jin616@users.noreply.github.com>
2023-03-27 17:41:15 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 164 ms, Memory: 5332 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
@@ -9,11 +9,12 @@ int is_prime(int n) {
     return 1;
 }
 
-void set_prime(int *arr) {
+void set_prime(int *arr, int *prime_arr) {
     int count = 0, k, tem;
     for (int i = 2; i < 1000000; i++) {
         if (arr[i] == 0 && is_prime(i)) {
-            count++;
+			prime_arr[count] = i;
+			count++;
             k = 1;
             while (i < 1000 && i * k <= 1000000) {
                 if (arr[i * k] == 0)
@@ -28,20 +29,20 @@ void set_prime(int *arr) {
 }
 
 int main() {
-    int t, n, tem, count, *arr;
+    int t, n, tem, count, *arr, *prime_arr;
 	arr = (int *)malloc(sizeof(int) * 1000000);
+	prime_arr = (int *)malloc(sizeof(int) * 78498);
     scanf("%d", &t);
-    set_prime(arr);
+    set_prime(arr, prime_arr);
     for (int i = 0; i < t; i++) {
         scanf("%d", &n);
-        tem = n / 2 - 1;
         count = 0;
-        while (++tem < n - 1) {
-            if(arr[tem] == 1 && arr[n - tem] == 1)
+        for (int j = 0; prime_arr[j] <= n / 2; j++)
+            if(arr[n - prime_arr[j]] == 1)
                 count++;
-		}
         printf("%d\n", count);
     }
 	free(arr);
+	free(prime_arr);
     return 0;
 }
