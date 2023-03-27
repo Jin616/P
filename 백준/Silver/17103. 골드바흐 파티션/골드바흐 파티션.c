17287c7815656e574f4480f90f88eb0c7cd1f145
jin616 <107658586+Jin616@users.noreply.github.com>
2023-03-27 18:01:06 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 12 ms, Memory: 5328 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
@@ -1,48 +1,31 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 
-int is_prime(int n) {
-    for (int i = 2; i <= n / i; i++)
-        if (n % i == 0)
-            return 0;
-    return 1;
-}
+int arr[1000000];
+int prime_arr[78498];
 
 void set_prime(int *arr, int *prime_arr) {
-    int count = 0, k, tem;
-    for (int i = 2; i < 1000000; i++) {
-        if (arr[i] == 0 && is_prime(i)) {
-			prime_arr[count] = i;
-			count++;
-            k = 1;
-            while (i < 1000 && i * k <= 1000000) {
-                if (arr[i * k] == 0)
-					arr[i * k] = 2;
-				k++;
-			}
-            arr[i] = 1;
-        }
-        else if (arr[i] == 0)
-            arr[i] = 2;
-    }
+    int count = 0;
+	arr[0] = arr[1] = 1;
+    for (int i = 2; i < 1000; i++)
+        if (!arr[i])
+            for (int j = i + i; j <= 1000000; j += i)
+				arr[j] = 1;
+	for (int i = 2; i < 1000000; i++)
+		if (!arr[i])
+			prime_arr[count++] = i;
 }
 
 int main() {
-    int t, n, tem, count, *arr, *prime_arr;
-	arr = (int *)malloc(sizeof(int) * 1000000);
-	prime_arr = (int *)malloc(sizeof(int) * 78498);
+    int t, n, count;
     scanf("%d", &t);
     set_prime(arr, prime_arr);
     for (int i = 0; i < t; i++) {
         scanf("%d", &n);
         count = 0;
         for (int j = 0; prime_arr[j] <= n / 2; j++)
-            if(arr[n - prime_arr[j]] == 1)
+            if(!arr[n - prime_arr[j]])
                 count++;
         printf("%d\n", count);
     }
-	free(arr);
-	free(prime_arr);
     return 0;
 }
