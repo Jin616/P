288ae34366dcb6b90df3d86f253c364f95474bf6
jin616 <107658586+Jin616@users.noreply.github.com>
2023-03-27 18:10:24 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 12 ms, Memory: 2400 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int arr[1000000];
+bool arr[1000000];
 int prime_arr[78498];
+int count;
 
 void set_prime() {
-    int count = 0;
 	arr[0] = arr[1] = 1;
     for (int i = 2; i < 1000; i++)
         if (!arr[i])
             for (int j = i + i; j <= 1000000; j += i)
-				arr[j] = 1;
+				arr[j] = 1;	
+	count = 0;
 	for (int i = 2; i < 1000000; i++)
 		if (!arr[i])
 			prime_arr[count++] = i;
 }
 
 int main() {
-    int t, n, count;
+    int t, n;
     scanf("%d", &t);
     set_prime();
     for (int i = 0; i < t; i++) {
