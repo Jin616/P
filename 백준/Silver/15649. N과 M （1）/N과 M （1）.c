15fcb947dc6bfe4ff760a2895e0cb3b3d2c411ad
jin616 <107658586+Jin616@users.noreply.github.com>
2023-03-28 12:25:11 +0900

    [Silver III] Title: N과 M (1), Time: 4 ms, Memory: 2092 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.c
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
+char buf[1000000];
+int idx;
+
 void dep(int d, int m, int n, int *arr) {
     if (d > m) {
-        for (int i = 1; i <= m; i++)
-            printf("%d ", arr[i]);
-        printf("\n");
-        return;
+        for (int i = 1; i <= m; i++) {
+            buf[idx++] = arr[i] + '0';
+            buf[idx++] = ' ';
+        }
+        buf[idx++] = '\n';
+    } else {
+        for (int i = 1; i <= n; i++) {
+            arr[d] = i;
+            for (int j = 1; j < d; j++)
+                if (arr[j] == i) {
+                    arr[d] = 0;
+                    break;
+                }
+            if (arr[d])
+                dep(d + 1, m, n, arr);
+        }
     }
-    for (int i = 1; i <= n; i++) {
-        arr[d] = i;
-        for (int j = 1; j < d; j++)
-            if (arr[j] == i) {
-                arr[d] = 0;
-                break;
-            }
-        if (arr[d])
-            dep(d + 1, m, n, arr);
-    }
-    return;
 }
 
 int main() {
     int n, m;
+    idx = 0;
     scanf("%d %d", &n, &m);
     int arr[m + 1];
     dep(1, m, n, arr);
+    printf("%s", buf);
     return 0;
 }
