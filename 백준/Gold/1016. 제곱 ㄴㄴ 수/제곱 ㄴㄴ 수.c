3f5e2c2c02861a5f6a1d6038f5e622649d23c8ee
jin616 <107658586+Jin616@users.noreply.github.com>
2023-03-28 19:21:15 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 4 ms, Memory: 2092 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.c
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.c
@@ -9,10 +9,8 @@ int main() {
     scanf("%lld%lld", &min, &max);
 	for (i = 2; i*i <= max; i++)
 		for (j = ceil((double)min/(i*i)); i*i*j <= max; j++)
-			if(i*i*j >= min && !arr[i*i*j - min]) {
+			if(i*i*j >= min && !arr[i*i*j - min] && ++count)
 				arr[i*i*j - min] = 1;
-				count++;
-			}
 	printf("%lld\n", max - min - count + 1, count);
 	return 0;
 }
