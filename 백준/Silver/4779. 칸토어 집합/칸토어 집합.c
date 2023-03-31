a19c5e452625792d735d5192bab26c49ec188913
jin616 <107658586+Jin616@users.noreply.github.com>
2023-04-01 01:37:54 +0900

    [Silver III] Title: 칸토어 집합, Time: 0 ms, Memory: 1472 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.c
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.c
@@ -2,29 +2,36 @@
 #include <math.h>
 #include <string.h>
 
-char buf[531442];
+char buf[177148];
 int idx;
 
-void cantor(int dep, int n) {
-    if (dep != 0) {
-        cantor(dep - 1, n);
+void cantor(int dep) {
+    if (dep > 0) {
+        cantor(dep - 1);
         idx += (int)pow(3, dep - 1);
-        cantor(dep - 1, n);
+        cantor(dep - 1);
     }
     else
         buf[idx++] = '-';
 }
 
 int main() {
-    int n;
-    idx = 0;
+    int n, pown;
     while (scanf("%d", &n) != -1) {
-        memset(buf, ' ', sizeof(buf));
-        cantor(n, n);
-        buf[idx++] = '\n';
+        if (n == 0) {
+            printf("-\n");
+            continue;
+        }
+        memset(buf, ' ', sizeof(buf) - 1);
+        idx = 0;
+        cantor(n - 1);
         buf[idx] = 0;
         printf("%s", buf);
+        memset(buf, ' ', idx);
+        printf("%s", buf);
         idx = 0;
+        cantor(n - 1);
+        printf("%s\n", buf);
     }
     return 0;
 }
