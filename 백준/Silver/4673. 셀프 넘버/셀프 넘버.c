9a75cf849c089d5f633a32e0df82a89c2ecd515c
jin616 <107658586+Jin616@users.noreply.github.com>
2023-04-01 00:18:10 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.c
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.c
@@ -1,36 +1,20 @@
 #include <stdio.h>
 
-char buf[50000];
-char selfnum[100001]; // 0 is self num
-int idx;
-
-void buf_write(int i) {
-	if (i > 0) {
-		buf_write(i / 10);
-		buf[idx++] = i % 10 + '0';
-	}
-}
-
-void is_selfnum(int i) {
-	int tem;
-	tem = i;
-	while (i > 0) {
-		tem += i % 10;
-		i /= 10;
-	}
-	if (tem <= 10000 && !selfnum[tem]++)
-		is_selfnum(tem);
-}
+char selfnum[10001]; // 0 is self num
 
 int main() {
-	idx = 0;
+	int tem, t;
     for (int i = 1; i <= 10000; i++) {
-		if (selfnum[i])
-			continue;
-		buf_write(i);
-		buf[idx++] = '\n';
-        is_selfnum(i);
-    }
-	printf("%s", buf);
+		tem = i;
+		if (!selfnum[i])
+			printf("%d\n", i);
+		while (!selfnum[tem]++ && tem <= 10000) {
+			t = tem;
+			while (t > 0) {
+				tem += t % 10;
+				t /= 10;
+			}
+		}
+	}
 	return 0;
 }
