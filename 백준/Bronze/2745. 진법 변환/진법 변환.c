3c31484dbcb2591fb0fe1ba6f83dc0b334dd3737
jin616 <107658586+Jin616@users.noreply.github.com>
2023-04-03 21:56:37 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char num[33];
+    int n = 0, b;
+    scanf("%s%d", num, &b);
+    for (int i = 0; num[i]; i++)
+        if (num[i] >= 'A')
+            n = b*n + num[i] - 'A' + 10;
+        else
+            n = b*n + num[i] - '0';
+    printf("%d", n);
+    return 0;
+}
