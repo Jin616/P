4fa1554bec309dd4770f132eebac2e5b7e6e71dd
jin616 <107658586+Jin616@users.noreply.github.com>
2023-04-08 14:06:29 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.c
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int t, n, arr[41][2];
+    int t, n, arr[41];
     scanf("%d", &t);
-    arr[0][0] = 1; arr[0][1] = 0;
-    arr[1][0] = 0; arr[1][1] = 1;
-    for (int i = 2; i < 41; i++) {
-        arr[i][0] = arr[i-1][0] + arr[i-2][0];
-        arr[i][1] = arr[i-1][1] + arr[i-2][1];
-    }
+    arr[0] = 1; arr[1] = 1;
+    for (int i = 2; i < 41; i++)
+        arr[i] = arr[i-1] + arr[i-2];
     for (int i = 0; i < t; i++) {
         scanf("%d", &n);
-        printf("%d %d\n", arr[n][0], arr[n][1]);
+        if(n <= 1)
+            printf("%d %d\n", 1-n, n);
+        else
+            printf("%d %d\n", arr[n-2], arr[n-1]);
     }
     return 0;
 }
